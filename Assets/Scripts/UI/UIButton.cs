91cff18b6f006c3a1f89c9bedc68260fec06b9f5
NikiBober <95879723+NikiBober@users.noreply.github.com>
2023-04-22 15:39:09 +0300

    v 0.8 Loading screen
    -1 hr-

--- Assets/Scripts/UI/UIButton.cs
+++ Assets/Scripts/UI/UIButton.cs
@@ -1,12 +1,25 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIButton : MonoBehaviour
 {
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private Slider _progressBar;
+    [SerializeField] private TextMeshProUGUI _progressText;
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
     public void LoadScene(string sceneToLoad)
     {
-        SceneManager.LoadScene(sceneToLoad);
+        _loadingScreen.SetActive(true);
+        StartCoroutine(LoadSceneRoutine(sceneToLoad));
     }
 
     public void Exit()
@@ -16,5 +29,20 @@ public class UIButton : MonoBehaviour
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #endif
+    }
+
+    private IEnumerator LoadSceneRoutine(string sceneToLoad)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        while (!asyncLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f) * 100;
+
+            _progressBar.value = progress;
+            _progressText.text = $"Loading... {Mathf.Round(progress)}%";
+
+            yield return null;
+        }
     }
 }
